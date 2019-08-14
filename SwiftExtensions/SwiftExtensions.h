062db338fca18bdd99f2b23dec04fe802d31b467
Igor Malyarov <photo.igor@mac.com>
2019-08-14 08:16:10 +0300

    Initial Commit

---
+++ SwiftExtensions/SwiftExtensions.h
@@ -0,0 +1,19 @@
+//
+//  SwiftExtensions.h
+//  SwiftExtensions
+//
+//  Created by Igor Malyarov on 14.08.2019.
+//  Copyright © 2019 Igor Malyarov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftExtensions.
+FOUNDATION_EXPORT double SwiftExtensionsVersionNumber;
+
+//! Project version string for SwiftExtensions.
+FOUNDATION_EXPORT const unsigned char SwiftExtensionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftExtensions/PublicHeader.h>
+
+
